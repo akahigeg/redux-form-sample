24764a517e7b4395eb53a009eb0cc8672f88656f
akahigeg <akahigeg@gmail.com>
2019-06-14 21:09:39 +0900

    Reduxを使う準備

---
+++ src/components/SimpleForm.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react'
+import * as ReduxForm from 'redux-form'
+
+// interface SimpleFormProps {
+//   name: string; // ?
+
+// }
+
+// interface SimpleFormData {
+//   firstName: string;
+//   lastName: string;
+// }
+
+const SimpleForm = (props: any)  => {
+  const { handleSubmit, pristine, reset, submitting } = props
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>First Name</label>
+        <div>
+          <ReduxForm.Field
+            name="firstName"
+            component="input"
+            type="text"
+            placeholder="First Name"
+          />
+        </div>
+      </div>
+      <div>
+        <label>Last Name</label>
+        <div>
+          <ReduxForm.Field
+            name="lastName"
+            component="input"
+            type="text"
+            placeholder="Last Name"
+          />
+        </div>
+      </div>
+      <div>
+        <button type="submit" disabled={pristine || submitting}>
+          Submit
+        </button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>
+          Clear Values
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default ReduxForm.reduxForm({
+  form: 'simple' // a unique identifier for this form
+})(SimpleForm)
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import App from "./App";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+
+const initialState = {
+  form: {}
+};
+
+const store = createStore(rootReducer, initialState);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root") as HTMLElement
 );
 registerServiceWorker();
