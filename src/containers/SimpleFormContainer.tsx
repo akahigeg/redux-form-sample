c71446261ff8314bd697547999ced3816007bb32
akahigeg <akahigeg@gmail.com>
2019-06-14 21:26:37 +0900

    サンプルのSimpleFormのフィールドをfirstNameとlastNameにしたものが動くところまで

---
+++ src/containers/SimpleFormContainer.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import SimpleForm from "../components/SimpleForm";
+
+class SimpleFormContainer extends React.Component {
+  submit = (values: any) => {
+    // print the form values to the console
+    console.log(values)
+  }
+  render() {
+    return <SimpleForm onSubmit={this.submit} />
+  }
+}
+
+// 自分でconnectしなくていいみたい
+export default SimpleFormContainer;
